99c209ea18a4a6c7c8683152d77c77a3ec76319a
Sampath44 <sampathkumar.u99@gmail.com>
2022-07-22 15:36:42 +0530

    my commit

---
+++ src/main/java/com/infy/application/InfyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.infy.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGatewayApplication.class, args);
+	}
+
+}
